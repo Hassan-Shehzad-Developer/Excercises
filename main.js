cca8330e6316a2a3dc84e9d785b24c403824eec1
Hassan Shehzad <hassanshehzad2671@gmail.com>
2024-02-23 06:56:01 +0500

    message

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 // Excercise no 1
-// Installed
-// i. Install [Node.js] (https://nodejs.org/en/),
-// ii. [TypeScript] (https://www.typescriptlang.org/download)
-// iii. VS Code (https://www.code.visualstudio.com)
-// install on your computer.
-// Excercise no 2
+// Install = Node-js, Typescript, VS code on your computer
+// Done.
+// Excercise no 2 
 // Personal Message:
+console.log("Hello World project 1");
 var personName = "Hassan";
 console.log("Hello Hassan,would you like to learn some Python today? ");
-console.log("Hello World project 1");
--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 // Excercise no 1
-// Installed
-// i. Install [Node.js] (https://nodejs.org/en/),
-// ii. [TypeScript] (https://www.typescriptlang.org/download)
-// iii. VS Code (https://www.code.visualstudio.com)
-// install on your computer.
 
-// Excercise no 2
+// Install = Node-js, Typescript, VS code on your computer
+// Done.
+
+
+// Excercise no 2 
+
 // Personal Message:
-let personName: string = "Hassan";
-console.log("Hello Hassan,would you like to learn some Python today? ");
+
 console.log("Hello World project 1");
+let personName: string = "Hassan";console.log("Hello Hassan,would you like to learn some Python today? ");
