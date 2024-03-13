8135b892bd01255f80e17281fc76801ab6f96f01
Hassan Shehzad <hassanshehzad2671@gmail.com>
2024-03-13 05:32:52 +0500

    update

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// Excercise no 2:
-// Personal Message: Store a person’s name in a variable, and print a message to that person.
-// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-{
-    var personName = "Hassan";
-    console.log("Hello Hassan,would you like to learn some Python today? ");
-}
 // Excercise no 3
 //  Name Cases:
 // lowerCase
