69d94c3dceea1f364e7fa133fea640a4e87b97cc
Hassan Shehzad <hassanshehzad2671@gmail.com>
2024-03-13 05:25:36 +0500

    update2

--- Question 1/main.ts
+++ Question 1/main.ts
@@ -1,2 +1,2 @@
 // Excercise no 1: Install Node.js ,TypeScript, VS Code install on your computer.
-// I have installed Java, TypeScript, and Visual Studio Code. on your computer
+// I have installed Java, TypeScript, and Visual Studio Code. on my computer
