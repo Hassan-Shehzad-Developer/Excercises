8f81072638c867411da5b03a2bf4a40c94f6d3d9
Hassan Shehzad <hassanshehzad2671@gmail.com>
2024-03-15 18:34:16 +0500

    update

--- Question 5/main.ts
+++ Question 5/main.ts
@@ -1,1 +1,32 @@
 // Excercise no 5
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called 
+// famous_person.Then compose your message and store it in a new variable called message. Print your message.
+
+
+//   {
+
+//     let famous_person = "Albert Einstein once said ";
+//     let quote = "A person who never made a mistake never tried anything new";
+//     let message = famous_person + quote;
+//     console.log(message);
+//   }
+
+
+{
+    let famous_person = "Winston Churchill once said ";
+    let quote = "Success is not final, failure is not fatal: It is the courage to continue that counts";
+    let message = famous_person   +  quote ;
+    console.log(message)
+}
+
+
+
+
+
+
+
+
+
+
+
+
