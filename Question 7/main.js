5a49b9f91a5fa33efa32e8aff366730b778eb413
Hassan Shehzad <hassanshehzad2671@gmail.com>
2024-03-17 20:05:52 +0500

    update

---
+++ Question 7/main.js
@@ -0,0 +1,31 @@
+//  Excercise no 7
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each 
+// result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// Addition:
+{
+    var a = 5;
+    var b = 3;
+    var add = a + b;
+    console.log("Addition:  5 + 3 =", add);
+}
+{
+    var a = 15;
+    var b = 7;
+    var sub = a - b;
+    console.log("Subtraction: 15 - 7 =", sub);
+}
+{
+    var a = 4;
+    var b = 2;
+    var multi = a * b;
+    console.log("Multiplication: 4 * 2 =", multi);
+}
+{
+    var a = 72;
+    var b = 9;
+    var divi = 72 / 9;
+    console.log("Division: 72 / 9 =", divi);
+}
+// console.log("Subtraction:",`, sub)
+// console.log("Multiplication:",`multiplication=`, multi); 
+//  console.log("Division:",`division=`, divi);
---
+++ Question 7/main.ts
@@ -0,0 +1,36 @@
+//  Excercise no 7
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each 
+// result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+// Addition:
+{
+let a = 5
+let b = 3
+let add = a + b;
+console.log("Addition:  5 + 3 =", add) 
+}
+{let a = 15
+ let b = 7
+ let sub = a - b;
+ console.log("Subtraction: 15 - 7 =",sub)
+}
+{
+let a = 4
+let b = 2
+let multi = a * b;
+console.log("Multiplication: 4 * 2 =",multi)
+}
+{
+let a = 72
+let b = 9
+let divi = 72 / 9
+console.log("Division: 72 / 9 =",divi)
+
+}
+      
+// console.log("Subtraction:",`, sub)
+// console.log("Multiplication:",`multiplication=`, multi); 
+//  console.log("Division:",`division=`, divi);
+
+
+
