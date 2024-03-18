07a932915e30660ca0d974b42ba2b7e358a08dde
Hassan Shehzad <hassanshehzad2671@gmail.com>
2024-03-18 09:12:06 +0500

    upload

---
+++ Question 11/main.js
@@ -0,0 +1,6 @@
+// Exercise no 11
+// Names: Store the names of a few of your friends in a array called names. Print 
+// each person’s name by accessing each element in the list, one at a time.
+var names = ["Ramzan", "Sheraz", "David", "Charli", "Daniyal"];
+for (var number = 0; number < names.length; number++)
+    console.log(names[number]);
---
+++ Question 11/main.ts
@@ -0,0 +1,15 @@
+// Exercise no 11
+// Names: Store the names of a few of your friends in a array called names. Print 
+// each person’s name by accessing each element in the list, one at a time.
+
+
+let names = ["Ramzan","Sheraz","David","Charli","Daniyal"] 
+
+for (let number = 0; number < names.length; number++) 
+
+console.log (names[number]);
+
+
+
+
+
