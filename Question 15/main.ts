6d9da9474d84a9cb74dc83f48adcd8f5512be75b
Hassan Shehzad <hassanshehzad2671@gmail.com>
2024-03-31 14:15:45 +0500

    update

--- Question 15/main.ts
+++ Question 15/main.ts
@@ -3,18 +3,24 @@
 //  to send out a new set of invitations. You’ll have to think of someone else to invite.
 
 {
+    // Old Guest List
   let guestlist = ["Ramzan", "Sheraz", "Shan"];
   console.log("Guest List");
 
   for (let i = 0; i < guestlist.length; i++) {
-    console.log(guestlist[i]);
+ console.log(`Hey ${guestlist[i]}, you are cordially invited to dinner with me.`);
   }
 
   console.log(`unfortunatly ${guestlist[2]} wil not come in dinner due to some reason`);
 
+//   New Updated Guest List
+
+console.log("New Update Guest List");
+
 guestlist[2]= "Daniyal";
 for (let i = 0; i < guestlist.length; i++) {
-    console.log(guestlist[i]);
+    
+    console.log(`Hey ${guestlist[i]}, you are cordially invited to dinner with me.`);
 
 }
 }
