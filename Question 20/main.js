fb9ef73c8e9b797d5272b021afe1e54ba8e18884
Hassan Shehzad <hassanshehzad2671@gmail.com>
2024-04-01 23:18:58 +0500

    solved

---
+++ Question 20/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+{
+    // Creating an array of Languages.
+    let languages = ["English", "Urdu", "Punjabi", "Sindhi", "Bangali", "Pashto"];
+    // Printing the list of mountains
+    console.log("list of languages");
+    for (let i = 0; i < languages.length; i++) {
+        console.log(languages[i]);
+    }
+}
