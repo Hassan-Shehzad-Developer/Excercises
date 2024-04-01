bdea07104e369dc4f7b387887450eb1f44c8a928
Hassan Shehzad <hassanshehzad2671@gmail.com>
2024-04-01 10:22:59 +0500

    solved

--- Question 17/main.js
+++ Question 17/main.js
@@ -1,4 +1,5 @@
 "use strict";
+// Exercise no 17
 // Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and 
 // you have space for only two guests.
 // • Start with your program from Exercise 16. Add a new line that prints a message saying that you can invite
---
+++ Question 19/main.ts
@@ -0,0 +1,12 @@
+// Exercise no 19
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+{
+// Here i am working on program number 14 to print a message indicating the number of people i am inviting to dinner.
+let guestlist = ["Ramzan", "Sheraz", "Shan"];
+for (let i = 0; i < guestlist.length; i++) {
+    let guest = guestlist[i];
+    console.log(`Hey ${guest}, you are cordially invited to dinner with me.`);
+}
+
+console.log(`Total number of guests invited to dinner: ${guestlist.length}`);
+}
