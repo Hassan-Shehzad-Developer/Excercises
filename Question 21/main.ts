d16e4235fdeb2b162662d7d6f8406881337cb6e6
Hassan Shehzad <hassanshehzad2671@gmail.com>
2024-04-02 19:39:50 +0500

    solved

---
+++ Question 21/main.ts
@@ -0,0 +1,17 @@
+// Exercise no 21:
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+// Define TypeScript objects for storing information about Students:
+let StudentResult = [
+  { name: "Hassan", Class: "10th", SecuredMarks: 88, Grade: "A" },
+  { name: "Sheraz", Class: "10th", SecuredMarks: 90, Grade: "A" },
+  { name: "Ramzan", Class: "10th", SecuredMarks: 78, Grade: "B" },
+  { name: "Salamn", Class: "10th", SecuredMarks: 65, Grade: "C" },
+];
+console.log(StudentResult);
+
+// Print information about each Student:
+
+StudentResult.forEach(StudentResult => {
+console.log(`StudentResult: ${StudentResult.name}, Class: ${StudentResult.Class}, SecuredMarks: ${StudentResult.SecuredMarks}, Grade: ${StudentResult.Grade}`)});
+
