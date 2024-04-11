775abb7f8a4abb45da6fcc69facaf0f64794cbc0
Hassan Shehzad <hassanshehzad2671@gmail.com>
2024-04-11 23:13:07 +0500

    solved

---
+++ Question 27/main.js
@@ -0,0 +1,43 @@
+"use strict";
+// Exercise no 27
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • If the alien is green, print a message that the player earned 5 points.
+// • If the alien is yellow, print a message that the player earned 10 points.
+// • If the alien is red, print a message that the player earned 15 points.
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+// Choose a color for the first alien
+let aliencolor = "green";
+// If-else chain to determine points earned for the first alien
+if (aliencolor === "green") {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (aliencolor === "yellow") {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (aliencolor === "red") {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
+// Choose a color for the second alien
+aliencolor = "yellow";
+// If-else chain to determine points earned for the second alien
+if (aliencolor === "green") {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (aliencolor === "yellow") {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (aliencolor === "red") {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
+// Choose a color for the third alien
+aliencolor = "red";
+// If-else chain to determine points earned for the third alien
+if (aliencolor === "green") {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (aliencolor === "yellow") {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (aliencolor === "red") {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
